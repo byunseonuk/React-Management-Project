887c3bd94f0a59f1fc09db4a1bc3328cd5c519a4
byunseonuk <byunseonuk@naver.com>
2020-05-17 07:50:31 +0000

    mysql database connection

--- client/src/App.js
+++ client/src/App.js
@@ -8,69 +8,72 @@ import TableCell from '@material-ui/core/TableCell';
 import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
     root: {
        width: '100%',
-       marginTop: theme.spacing.unit*3,
+       marginTop: theme.spacing(3),
        overflow: 'auto'
     },
     table: {
         minWidth: 1080
+    },
+    progress: {
+        marginTop: theme.spacing(2)
     }
 });
-const customers = [
-{
-    'id':1,
-    'image':'https://placeimg.com/64/64/1',
-    'name' : '홍길동',
-    'birthday':'200504',
-    'gender':'남자',
-    'job':'대학생',
-},
-{
-    'id':2,
-    'image':'https://placeimg.com/64/64/2',
-    'name' : '임꺽정',
-    'birthday':'800504',
-    'gender':'남자',
-    'job':'개발자',
-},
-{
-    'id':3,
-    'image':'https://placeimg.com/64/64/3',
-    'name' : '신데렐라',
-    'birthday':'180504',
-    'gender':'여자',
-    'job':'대학생',
-}
-]
+
 
 class App extends Component {
+    
+  state = {
+      customers: "",
+      completed: 0
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+      const response = await fetch('/api/customers');
+      const body = await response.json();
+      return body;
+  }
+  
+  progress = () => {
+      const { completed } = this.state;
+      this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+  
   render(){
     const { classes } = this.props;
     return (
         <Paper className={classes.root}>
             <Table className={classes.table}>
                 <TableHead>
-                    <TableCell>번호</TableCell>
-                    <TableCell>이미지</TableCell>
-                    <TableCell>이름</TableCell>
-                    <TableCell>생년월일</TableCell>
-                    <TableCell>성별</TableCell>
-                    <TableCell>직업</TableCell>
+                    <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell>이미지</TableCell>
+                        <TableCell>이름</TableCell>
+                        <TableCell>생년월일</TableCell>
+                        <TableCell>성별</TableCell>
+                        <TableCell>직업</TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                { customers.map(c => {
-                        return(
-                            <Customer
-                                key={c.id} id={c.id} image={c.image} name={c.name} 
-                                birthday={c.birthday} gender={c.gender} job={c.job}
-
-                            /> 
-                        )
-                    })
-                }
+                    { this.state.customers ? this.state.customers.map(c => {
+                            return( <Customer key={c.id} id={c.id} image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job} /> )
+                    }) : 
+                    <TableRow>
+                        <TableCell colSpan="6" align="center">
+                            <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                        </TableCell>
+                    </TableRow>}
                 </TableBody>
             </Table>
         </Paper>
